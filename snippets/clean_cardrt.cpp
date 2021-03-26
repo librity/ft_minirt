03ed5fc4f714b18d183941fad0152baad6cf413e
Luis Geniole <luis.geniole@gmail.com>
2021-03-26 03:47:49 -0300

    Add minilibx demos.

--- snippets/clean_cardrt.cpp
+++ snippets/clean_cardrt.cpp
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 02:53:29 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2021/03/26 03:14:32 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2021/03/26 03:35:06 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,21 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef int i;
-typedef float f;
-
 struct vector_3d
 {
-	f x, y, z;
+	float x, y, z;
 
 	vector_3d operator+(vector_3d r)
 	{
 		return vector_3d(x + r.x, y + r.y, z + r.z);
 	}
 
-	vector_3d operator*(f r)
+	vector_3d operator*(float r)
 	{
 		return vector_3d(x * r, y * r, z * r);
 	}
 
-	f operator%(vector_3d r)
+	float operator%(vector_3d r)
 	{
 		return x * r.x + y * r.y + z * r.z;
 	}
@@ -51,7 +48,7 @@ struct vector_3d
 		return vector_3d(y * r.z - z * r.y, z * r.x - x * r.z, x * r.y - y * r.x);
 	}
 
-	vector_3d(f a, f b, f c)
+	vector_3d(float a, float b, float c)
 	{
 		x = a;
 		y = b;
@@ -64,36 +61,36 @@ struct vector_3d
 	}
 };
 
-i G[] = {247570, 280596, 280600, 249748, 18578, 18577, 231184, 16, 16};
+int G[] = {247570, 280596, 280600, 249748, 18578, 18577, 231184, 16, 16};
 
-f R()
+float R()
 {
-	return (f)rand() / RAND_MAX;
+	return (float)rand() / RAND_MAX;
 }
 
-i T(vector_3d o, vector_3d d, f &t, vector_3d &n)
+int T(vector_3d o, vector_3d d, float &t, vector_3d &n)
 {
 	t = 1e9;
-	i m = 0;
-	f p = -o.z / d.z;
+	int m = 0;
+	float p = -o.z / d.z;
 
 	if (.01 < p)
 	{
 		t = p, n = vector_3d(0, 0, 1), m = 1;
 	}
 
-	for (i k = 19; k--;)
+	for (int k = 19; k--;)
 	{
-		for (i j = 9; j--;)
+		for (int j = 9; j--;)
 		{
 			if (G[j] & 1 << k)
 			{
 				vector_3d p = o + vector_3d(-k, 0, -j - 4);
-				f b = p % d, c = p % p - 1, q = b * b - c;
+				float b = p % d, c = p % p - 1, q = b * b - c;
 
 				if (q > 0)
 				{
-					f s = -b - sqrt(q);
+					float s = -b - sqrt(q);
 
 					if (s < t && s > .01)
 					{
@@ -109,9 +106,9 @@ i T(vector_3d o, vector_3d d, f &t, vector_3d &n)
 
 vector_3d S(vector_3d o, vector_3d d)
 {
-	f t;
+	float t;
 	vector_3d n;
-	i m = T(o, d, t, n);
+	int m = T(o, d, t, n);
 
 	if (!m)
 	{
@@ -119,42 +116,42 @@ vector_3d S(vector_3d o, vector_3d d)
 	}
 
 	vector_3d h = o + d * t, l = !(vector_3d(9 + R(), 9 + R(), 16) + h * -1), r = d + n * (n % d * -2);
-	f b = l % n;
+	float b = l % n;
 
 	if (b < 0 || T(h, l, t, n))
 	{
 		b = 0;
 	}
 
-	f p = pow(l % r * (b > 0), 99);
+	float p = pow(l % r * (b > 0), 99);
 
 	if (m & 1)
 	{
 		h = h * .2;
-		return ((i)(ceil(h.x) + ceil(h.y)) & 1 ? vector_3d(3, 1, 1) : vector_3d(3, 3, 3)) * (b * .2 + .1);
+		return ((int)(ceil(h.x) + ceil(h.y)) & 1 ? vector_3d(3, 1, 1) : vector_3d(3, 3, 3)) * (b * .2 + .1);
 	}
 
 	return vector_3d(p, p, p) + S(h, r) * .5;
 }
 
-i main()
+int main()
 {
 	printf("P6 512 512 255 ");
 	vector_3d g = !vector_3d(-6, -16, 0), a = !(vector_3d(0, 0, 1) ^ g) * .002, b = !(g ^ a) * .002, c = (a + b) * -256 + g;
 
-	for (i y = 512; y--;)
+	for (int y = 512; y--;)
 	{
-		for (i x = 512; x--;)
+		for (int x = 512; x--;)
 		{
 			vector_3d p(13, 13, 13);
 
-			for (i r = 64; r--;)
+			for (int r = 64; r--;)
 			{
 				vector_3d t = a * (R() - .5) * 99 + b * (R() - .5) * 99;
 				p = S(vector_3d(17, 16, 8) + t, !(t * -1 + (a * (R() + x) + b * (y + R()) + c) * 16)) * 3.5 + p;
 			}
 
-			printf("%c%c%c", (i)p.x, (i)p.y, (i)p.z);
+			printf("%c%c%c", (int)p.x, (int)p.y, (int)p.z);
 		}
 	}
 }
---
+++ snippets/minilibx_1.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilibx_1.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/26 03:39:38 by lpaulo-m          #+#    #+#             */
+/*   Updated: 2021/03/26 03:46:44 by lpaulo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// clang -I ../includes example.c -lbsd -lmlx -lXext -lX11 && ./a.out
+// gcc -I ../includes example.c -lbsd -lmlx -lXext -lX11 && ./a.out
+
+#include "mlx.h"
+#include "mlx_int.h"
