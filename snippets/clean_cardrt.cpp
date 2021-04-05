a7122842abdc2f18c9ee59c9369f9972e5f6948c
Luis Geniole <luis.geniole@gmail.com>
2021-04-04 22:59:03 -0300

    Add README images.

--- snippets/clean_cardrt.cpp
+++ snippets/clean_cardrt.cpp
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 02:53:29 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2021/03/26 03:35:06 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2021/04/04 22:51:03 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,7 @@ struct vector_3d
 		return x * r.x + y * r.y + z * r.z;
 	}
 
-	vector_3d()
-	{
-	}
+	vector_3d() {}
 
 	vector_3d operator^(vector_3d r)
 	{
@@ -63,12 +61,12 @@ struct vector_3d
 
 int G[] = {247570, 280596, 280600, 249748, 18578, 18577, 231184, 16, 16};
 
-float R()
+float random()
 {
 	return (float)rand() / RAND_MAX;
 }
 
-int T(vector_3d o, vector_3d d, float &t, vector_3d &n)
+int trace(vector_3d o, vector_3d d, float &t, vector_3d &n)
 {
 	t = 1e9;
 	int m = 0;
@@ -108,17 +106,17 @@ vector_3d S(vector_3d o, vector_3d d)
 {
 	float t;
 	vector_3d n;
-	int m = T(o, d, t, n);
+	int m = trace(o, d, t, n);
 
 	if (!m)
 	{
 		return vector_3d(.7, .6, 1) * pow(1 - d.z, 4);
 	}
 
-	vector_3d h = o + d * t, l = !(vector_3d(9 + R(), 9 + R(), 16) + h * -1), r = d + n * (n % d * -2);
+	vector_3d h = o + d * t, l = !(vector_3d(9 + random(), 9 + random(), 16) + h * -1), r = d + n * (n % d * -2);
 	float b = l % n;
 
-	if (b < 0 || T(h, l, t, n))
+	if (b < 0 || trace(h, l, t, n))
 	{
 		b = 0;
 	}
@@ -147,8 +145,8 @@ int main()
 
 			for (int r = 64; r--;)
 			{
-				vector_3d t = a * (R() - .5) * 99 + b * (R() - .5) * 99;
-				p = S(vector_3d(17, 16, 8) + t, !(t * -1 + (a * (R() + x) + b * (y + R()) + c) * 16)) * 3.5 + p;
+				vector_3d t = a * (random() - .5) * 99 + b * (random() - .5) * 99;
+				p = S(vector_3d(17, 16, 8) + t, !(t * -1 + (a * (random() + x) + b * (y + random()) + c) * 16)) * 3.5 + p;
 			}
 
 			printf("%c%c%c", (int)p.x, (int)p.y, (int)p.z);
