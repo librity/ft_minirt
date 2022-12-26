c51231b069b17d70dedb54812c86f7ba995a0a78
Luis <luis.geniole@gmail.com>
2022-12-26 13:42:50 -0300

    Test vector addition.
    Co-authored-by: Pedro Paulo do Nascimento <pedro-pn@users.noreply.github.com>

--- tests/algebra/vectors.c
+++ tests/algebra/vectors.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+              /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   vectors.c                                          :+:      :+:    :+:   */
@@ -6,13 +6,21 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 16:59:18 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/26 13:31:03 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/26 13:37:21 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../tests.h"
 
-int	foo;
+t_v3d	expected;
+t_v3d	result;
+
+void	assert_v3d_equals(t_v3d	_expected, t_v3d	_result)
+{
+	mu_assert_double_eq(_expected.x, _result.x);
+	mu_assert_double_eq(_expected.y, _result.y);
+	mu_assert_double_eq(_expected.z, _result.z);
+}
 
 void	test_setup(void)
 {
@@ -21,23 +29,28 @@ void	test_teardown(void)
 {
 }
 
-MU_TEST(template_tst)
+MU_TEST(add_tst)
 {
-	foo = 42;
-	mu_assert_int_eq(foo, 42);
+	result  = add(
+		(t_v3d){1.0, 2.0, 3.0},
+		(t_v3d){2.0, 3.0, 1.0}
+	);
+	expected = (t_v3d){3.0, 5.0, 4.0};
+
+	assert_v3d_equals(expected, result);
 }
 
-MU_TEST_SUITE(test_suite)
+MU_TEST_SUITE(vectors_suite)
 {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
-	MU_RUN_TEST(template_tst);
+	MU_RUN_TEST(add_tst);
 }
 
 MU_MAIN
 {
 	MU_DIVIDER;
-	MU_RUN_SUITE(test_suite);
+	MU_RUN_SUITE(vectors_suite);
 	MU_REPORT();
 	return (MU_EXIT_CODE);
 }
