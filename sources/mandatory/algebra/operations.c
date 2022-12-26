32c30d9d8f0a843e2c1db51a002615feba623dd3
Luis <luis.geniole@gmail.com>
2022-12-26 18:41:43 -0300

    Fix and test tuple subtraction.
    Co-authored-by: Pedro Paulo do Nascimento <pedro-pn@users.noreply.github.com>

--- sources/mandatory/algebra/operations.c
+++ sources/mandatory/algebra/operations.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 16:43:25 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/26 18:28:01 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/26 18:41:08 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,12 @@ t_t3d	sub(t_t3d first, t_t3d second)
 {
 	t_t3d	result;
 
+	if (first.type == VECTOR_TYPE && second.type == POINT_TYPE)
+		return (null_tuple());
 	result.x = first.x - second.x;
 	result.y = first.y - second.y;
 	result.z = first.z - second.z;
+	result.type = first.type - second.type;
 	return (result);
 }
 
--- tests/algebra/operations.c
+++ tests/algebra/operations.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 16:59:18 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/26 18:31:37 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/26 18:41:20 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_t3d expected;
 t_t3d result;
 
-void assert_tuple_equals(t_t3d _expected, t_t3d _result)
+void assert_tuple_eq(t_t3d _expected, t_t3d _result)
 {
 	mu_assert_double_eq(_expected.x, _result.x);
 	mu_assert_double_eq(_expected.y, _result.y);
@@ -34,11 +34,11 @@ MU_TEST(tuple_tst)
 {
 	result = tuple(1.0, 2.0, 3.0, VECTOR_TYPE);
 	expected = (t_t3d){1.0, 2.0, 3.0, VECTOR_TYPE};
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 
 	result = tuple(4.3, -4.2, 3.1, POINT_TYPE);
 	expected = (t_t3d){4.3, -4.2, 3.1, POINT_TYPE};
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 }
 
 MU_TEST(vector_tst)
@@ -46,7 +46,7 @@ MU_TEST(vector_tst)
 	result = vector(43.0, -341.94, 11.421);
 	expected = (t_v3d){43.0, -341.94, 11.421, VECTOR_TYPE};
 
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 }
 
 MU_TEST(point_tst)
@@ -54,7 +54,7 @@ MU_TEST(point_tst)
 	result = point(43.0, -341.94, 11.421);
 	expected = (t_p3d){43.0, -341.94, 11.421, POINT_TYPE};
 
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 }
 
 MU_TEST(add_tst)
@@ -63,35 +63,53 @@ MU_TEST(add_tst)
 		point(3, -2, 5),
 		vector(-2, 3, 1));
 	expected = point(1, 1, 6);
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 
 	result = add(
 		vector(-2, 3, 1),
 		point(3, -2, 5));
 	expected = point(1, 1, 6);
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 
 	result = add(
 		vector(1.0, -5.0, 4.0),
 		vector(3.0, 4.3, 2.0));
 	expected = vector(4, -0.7, 6.0);
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 
 	result = add(
 		point(1.0, -5.0, 4.0),
 		point(3.0, 4.3, 2.0));
 	expected = null_tuple();
-	assert_tuple_equals(expected, result);
+	assert_tuple_eq(expected, result);
 }
 
 MU_TEST(sub_tst)
 {
 	result = sub(
-		vector(1.0, 2.0, 3.0),
-		vector(2.0, 3.0, 1.0));
-	expected = vector(-1.0, -1.0, 2.0);
+		point(3, 2, 1),
+		point(5, 6, 7)
+	);
+	expected = vector(-2, -4, -6);
+	assert_tuple_eq(expected, result);
 
-	assert_tuple_equals(expected, result);
+	result = sub(
+		point(3, 2, 1),
+		vector(5, 6, 7));
+	expected = point(-2, -4, -6);
+	assert_tuple_eq(expected, result);
+
+	result = sub(
+		vector(5, 6, 7),
+		point(3, 2, 1));
+	expected = null_tuple();
+	assert_tuple_eq(expected, result);
+
+	result = sub(
+		vector(3, 2, 1),
+		vector(5, 6, 7));
+	expected = vector(-2, -4, -6);
+	assert_tuple_eq(expected, result);
 }
 
 MU_TEST_SUITE(vectors_suite)
@@ -102,7 +120,7 @@ MU_TEST_SUITE(vectors_suite)
 	MU_RUN_TEST(vector_tst);
 	MU_RUN_TEST(point_tst);
 	MU_RUN_TEST(add_tst);
-	// MU_RUN_TEST(sub_tst);
+	MU_RUN_TEST(sub_tst);
 }
 
 MU_MAIN
