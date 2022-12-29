9d0136bc8e0c32c54f7a87bf405d34339000237b
Luis <luis.geniole@gmail.com>
2022-12-29 17:00:03 -0300

    Improve save to .ppm performance.

--- sources/mandatory/mlx/save_ppm.c
+++ sources/mandatory/mlx/save_ppm.c
@@ -6,40 +6,69 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 15:45:29 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/29 16:13:30 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/29 16:58:55 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-static void	write_header(int fd, int width, int height)
+void	my_putstr_fd(char *str, int fd)
 {
-	char	*header;
+	size_t	length;
 
-	header = ft_strdup("P3\n");
-	header = ft_strjoin_free_free(header, ft_itoa(width));
-	header = ft_strjoin_free(header, " ");
-	header = ft_strjoin_free_free(header, ft_itoa(height));
-	header = ft_strjoin_free(header, "\n255\n");
-	ft_putstr_fd(header, fd);
-	free(header);
+	if (str == NULL)
+		return ;
+	length = ft_strlen(str);
+	write(fd, &c, length);
 }
 
-static void	write_color(int fd, t_rgb color)
+char	*my_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+		*dest++ = *src++;
+	*dest = '\0';
+	return (dest);
+}
+
+static char	*write_header(int width, int height, char *cursor)
+{
+	char	*width_s;
+	char	*height_s;
+
+	width_s = ft_itoa(width);
+	height_s = ft_itoa(height);
+	cursor = my_strcpy(cursor, "P3\n");
+	cursor = my_strcpy(cursor, width_s);
+	cursor = my_strcpy(cursor, " ");
+	cursor = my_strcpy(cursor, height_s);
+	cursor = my_strcpy(cursor, "\n255\n");
+	free(width_s);
+	free(height_s);
+	return (cursor);
+}
+
+static char	*write_color(t_rgb color, char *cursor)
 {
 	char	*pixel;
 
 	pixel = ft_itoa(color.red);
-	pixel = ft_strjoin_free(pixel, " ");
-	pixel = ft_strjoin_free_free(pixel, ft_itoa(color.green));
-	pixel = ft_strjoin_free(pixel, " ");
-	pixel = ft_strjoin_free_free(pixel, ft_itoa(color.blue));
-	pixel = ft_strjoin_free(pixel, " ");
-	ft_putstr_fd(pixel, fd);
+	cursor = my_strcpy(cursor, pixel);
+	cursor = my_strcpy(cursor, " ");
 	free(pixel);
+
+	pixel = ft_itoa(color.green);
+	cursor = my_strcpy(cursor, pixel);
+	cursor = my_strcpy(cursor, " ");
+	free(pixel);
+
+	pixel = ft_itoa(color.blue);
+	cursor = my_strcpy(cursor, pixel);
+	cursor = my_strcpy(cursor, " ");
+	free(pixel);
+	return (cursor);
 }
 
-static void	write_colors(t_mlx_image *image, int fd)
+static void	write_colors(t_mlx_image *image, char *cursor)
 {
 	int		x;
 	int		y;
@@ -52,21 +81,45 @@ static void	write_colors(t_mlx_image *image, int fd)
 		while (x < image->width)
 		{
 			color = mlx_image_get_pixel_rgb(image, x, y);
-			write_color(fd, color);
+			cursor = write_color(color, cursor);
 			x++;
 		}
-		ft_putstr_fd("\n", fd);
+		cursor = my_strcpy(cursor, "\n");
 		y++;
 		ft_putstr(".");
 	}
 }
 
+	// string_buffer = calloc(1, 10000);
+	// cursor = string_buffer;
+	//
+	// cursor = my_strcpy(cursor, "P3\n234 123\n255\n");
+	// "P3\n234 123\n255\n                                                                                            "
+	// cursor = my_strcpy(cursor, "234 13 123 ");
+ 	// "P3\n234 123\n255\n234 13 123                                                                                            "
+
+	// - [x] Allocar string buffer grande
+	// 		- [x] total = cabecalho + linhas * (colunas * 12 + 1) + 100
+	// - [x] Criar função para escrever na buffer e avançar o ponteiro
+	// - [x] Escrever o header na buffer
+	// - [ ] Escrever os pixies na buffer
+	// - [x] Escrever o string buffer no aquivo com um só write.
+
 void	mlx_image_save_ppm(t_mlx_image *image, char *filename)
 {
-	int	fd;
+	int		fd;
+	size_t	buffer_size;
+	char	*string_buffer;
+	char	*cursor;
+
+	buffer_size = 150 + image->height * (image->width * 12 + 1) + 100;
+	string_buffer = ft_calloc(1, buffer_size);
+	cursor = string_buffer;
+	cursor = write_header( image->width, image->height, cursor);
+	write_colors(image, cursor);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	write_header(fd, image->width, image->height);
-	write_colors(image, fd);
+	write(fd, string_buffer, ft_strlen(string_buffer));
+	free(string_buffer);
 	close(fd);
 }
