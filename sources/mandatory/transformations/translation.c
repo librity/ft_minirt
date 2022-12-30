b6b1e7e82730b44051d5a276b4019a8776016881
Luis <luis.geniole@gmail.com>
2022-12-30 17:05:13 -0300

    Implement scaling matrix function.
    Co-authored-by: Pedro Paulo do Nascimento <pedro-pn@users.noreply.github.com>

--- sources/mandatory/transformations/translation.c
+++ sources/mandatory/transformations/translation.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 16:35:38 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/30 16:53:19 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/30 17:05:00 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,12 @@ void	translation(t_v3d trans_vector, t_matrix *result)
 	(*result)[0][3] = trans_vector.x;
 	(*result)[1][3] = trans_vector.y;
 	(*result)[2][3] = trans_vector.z;
+}
+
+void	scaling(t_v3d vector, t_matrix *result)
+{
+	mx_set_identity(result);
+	(*result)[0][0] = vector.x;
+	(*result)[1][1] = vector.y;
+	(*result)[2][2] = vector.z;
 }
--- tests/transformations/translation.c
+++ tests/transformations/translation.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 16:36:15 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/30 16:51:22 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/30 17:01:35 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,34 @@ MU_TEST(vector_translation_tst)
 	assert_tuple_eq(expected, result);
 }
 
+MU_TEST(scaling_tst)
+{
+	scaling(vector(2, 3, 4), &mx_translation);
+	result = mx_tuple_multiply(mx_translation, point(-4, 6, 8));
+	expected = point(-8, 18, 32);
+
+	assert_tuple_eq(expected, result);
+}
+
+MU_TEST(vector_scaling_tst)
+{
+	scaling(vector(2, 3, 4), &mx_translation);
+	result = mx_tuple_multiply(mx_translation, vector(-4, 6, 8));
+	expected = vector(-8, 18, 32);
+
+	assert_tuple_eq(expected, result);
+}
+
+MU_TEST(inverse_scaling_tst)
+{
+	scaling(vector(2, 3, 4), &mx_translation);
+	mx_inverse(mx_translation, 4, &_mx_inverse);
+	result = mx_tuple_multiply(_mx_inverse, vector(-4, 6, 8));
+	expected = vector(-2, 2, 2);
+
+	assert_tuple_eq(expected, result);
+}
+
 MU_TEST_SUITE(translations_suite)
 {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
@@ -60,6 +88,10 @@ MU_TEST_SUITE(translations_suite)
 	MU_RUN_TEST(translation_tst);
 	MU_RUN_TEST(inverse_translation_tst);
 	MU_RUN_TEST(vector_translation_tst);
+
+	MU_RUN_TEST(scaling_tst);
+	MU_RUN_TEST(vector_scaling_tst);
+	MU_RUN_TEST(inverse_scaling_tst);
 }
 
 MU_MAIN
