99aad0edb59f3026d660609cc33dd359699b99e3
Luis <luis.geniole@gmail.com>
2023-01-26 19:52:41 -0300

    Handle above and below plane intersections with tests.
    Co-authored-by: Pedro Paulo do Nascimento <pedro-pn@users.noreply.github.com>

--- sources/mandatory/planes/intersect.c
+++ sources/mandatory/planes/intersect.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:37:48 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2023/01/26 19:45:08 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:52:20 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 t_intxs	intersect_plane(t_object *object, t_ray ray)
 {
 	t_intxs	result;
+	double	t;
 
 	(void)object;
 	result.count = 0;
 	result.list = NULL;
 	if (double_near_zero(ray.direction.y))
 		return (result);
+	t = -ray.origin.y / ray.direction.y;
+	result.count = 1;
+	create_intersection(&result.list, t, object);
 	return (result);
 }
--- tests/planes/intersect.c
+++ tests/planes/intersect.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 19:12:07 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2023/01/26 19:46:31 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:50:10 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 t_object	*_plane;
 t_ray		_ray;
 t_intxs		xs;
+t_intx		*inter;
 
 void	test_setup(void)
 {
@@ -45,12 +46,55 @@ MU_TEST(intersect_coplanar_tst)
 	mu_assert_int_eq(0, xs.count);
 }
 
+// Scenario: A ray intersecting a plane from above
+//  Given p ← plane()
+//  And r ← ray(point(0, 1, 0), vector(0, -1, 0))
+//  When xs ← local_intersect(p, r)
+//  Then xs.count = 1
+//  And xs[0].t = 1
+//  And xs[0].object = p
+MU_TEST(intersect_above_tst)
+{
+	_ray = ray(point(0, 1, 0), vector(0, -1, 0));
+	xs = intersect_object(_plane, _ray);
+
+	mu_check(xs.count > 0);
+	mu_check(xs.list != NULL);
+
+	inter = xs.list->content;
+	mu_assert_int_eq(1, inter->t);
+	mu_check(inter->object == _plane);
+}
+
+// Scenario: A ray intersecting a plane from below
+//  Given p ← plane()
+//  And r ← ray(point(0, -1, 0), vector(0, 1, 0))
+//  When xs ← local_intersect(p, r)
+//  Then xs.count = 1
+//  And xs[0].t = 1
+//  And xs[0].object = p
+
+MU_TEST(intersect_below_tst)
+{
+	_ray = ray(point(0, -1, 0), vector(0, 1, 0));
+	xs = intersect_object(_plane, _ray);
+
+	mu_check(xs.count > 0);
+	mu_check(xs.list != NULL);
+
+	inter = xs.list->content;
+	mu_assert_int_eq(1, inter->t);
+	mu_check(inter->object == _plane);
+}
+
 MU_TEST_SUITE(plane_intersect_suite)
 {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
 	MU_RUN_TEST(intersect_parallel_tst);
 	MU_RUN_TEST(intersect_coplanar_tst);
+	MU_RUN_TEST(intersect_above_tst);
+	MU_RUN_TEST(intersect_below_tst);
 }
 
 MU_MAIN
