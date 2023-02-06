1285a5c04cfa8c2dac1fbfe46db4ece758209d5d
Pedro Paulo <pe.pn03@gmail.com>
2023-02-06 12:06:57 -0300

    Fix uninitialized value in product result

--- sources/mandatory/algebra/operations.c
+++ sources/mandatory/algebra/operations.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 16:43:25 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/28 17:49:23 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2023/02/06 12:05:08 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ t_t3d	product(t_t3d first, t_t3d second)
 	result.x = first.x * second.x;
 	result.y = first.y * second.y;
 	result.z = first.z * second.z;
+	result.type = first.type * second.type;
 	return (result);
 }
 
