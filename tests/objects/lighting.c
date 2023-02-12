6c0f387684947bb9a38df94f7a76806c08e66d11
Luis <luis.geniole@gmail.com>
2023-02-12 19:36:11 +0100

    Fix tests.
    Co-authored-by: Pedro Paulo do Nascimento <pedro-pn@users.noreply.github.com>

--- tests/objects/lighting.c
+++ tests/objects/lighting.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 18:37:51 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2023/01/23 18:27:21 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2023/02/12 19:35:29 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ t_object	*_sphere;
 t_material	_material;
 t_p3d		_position;
 t_light		_light;
-t_v3d		eye;
-t_v3d		normal;
 t_c3d		expected_color;
 t_c3d		result_color;
+t_lighting_params	lighting_params;
 
 void	test_setup(void)
 {
@@ -33,10 +32,12 @@ void	test_teardown(void)
 
 MU_TEST(eye_tst)
 {
-	eye = vector(0, 0, -1);
-	normal = vector(0, 0, -1);
+	lighting_params.eye = vector(0, 0, -1);
+	lighting_params.normal = vector(0, 0, -1);
+	lighting_params.point = _position;
+	lighting_params.in_shadow = false;
 	_light = point_light(point(0, 0, -10), 1.0, color_rgb(255, 255, 255));
-	result_color = lighting(_material, _light, _position, eye, normal, false);
+	result_color = lighting(_material, _light, lighting_params);
 	expected_color = color_3d(1.9, 1.9, 1.9);
 
 	assert_tuple_eq(expected_color, result_color);
@@ -44,10 +45,12 @@ MU_TEST(eye_tst)
 
 MU_TEST(eye_fortyfive_degree_tst)
 {
-	eye = vector(0, sqrt(2) / 2, -sqrt(2) / 2);
-	normal = vector(0, 0, -1);
+	lighting_params.eye = vector(0, sqrt(2) / 2, -sqrt(2) / 2);
+	lighting_params.normal = vector(0, 0, -1);
+	lighting_params.in_shadow = false;
+	lighting_params.point = _position;
 	_light = point_light(point(0, 0, -10), 1.0, color_rgb(255, 255, 255));
-	result_color = lighting(_material, _light, _position, eye, normal, false);
+	result_color = lighting(_material, _light, lighting_params);
 	expected_color = color_3d(1.0, 1.0, 1.0);
 
 	assert_tuple_eq(expected_color, result_color);
@@ -55,10 +58,12 @@ MU_TEST(eye_fortyfive_degree_tst)
 
 MU_TEST(light_fortyfive_degree_tst)
 {
-	eye = vector(0, 0, -1);
-	normal = vector(0, 0, -1);
+	lighting_params.eye = vector(0, 0, -1);
+	lighting_params.normal = vector(0, 0, -1);
+	lighting_params.point = _position;
+	lighting_params.in_shadow = false;
 	_light = point_light(point(0, 10, -10), 1.0, color_rgb(255, 255, 255));
-	result_color = lighting(_material, _light, _position, eye, normal, false);
+	result_color = lighting(_material, _light, lighting_params);
 	expected_color = color_3d(0.7364, 0.7364, 0.7364);
 
 	assert_tuple_eq(expected_color, result_color);
@@ -66,10 +71,12 @@ MU_TEST(light_fortyfive_degree_tst)
 
 MU_TEST(light_ninety_degree_tst)
 {
-	eye = vector(0, -sqrt(2)/2, -sqrt(2)/2);
-	normal = vector(0, 0, -1);
+	lighting_params.eye = vector(0, -sqrt(2)/2, -sqrt(2)/2);
+	lighting_params.normal = vector(0, 0, -1);
+	lighting_params.in_shadow = false;
+	lighting_params.point = _position;
 	_light = point_light(point(0, 10, -10), 1.0, color_rgb(255, 255, 255));
-	result_color = lighting(_material, _light, _position, eye, normal, false);
+	result_color = lighting(_material, _light, lighting_params);
 	expected_color = color_3d(1.6364, 1.6364, 1.6364);
 
 	assert_tuple_eq(expected_color, result_color);
@@ -77,10 +84,12 @@ MU_TEST(light_ninety_degree_tst)
 
 MU_TEST(light_behind_tst)
 {
-	eye = vector(0, 0, -1);
-	normal = vector(0, 0, -1);
+	lighting_params.eye = vector(0, 0, -1);
+	lighting_params.normal = vector(0, 0, -1);
+	lighting_params.point = _position;
+	lighting_params.in_shadow = false;
 	_light = point_light(point(0, 0, 10), 1.0, color_rgb(255, 255, 255));
-	result_color = lighting(_material, _light, _position, eye, normal, false);
+	result_color = lighting(_material, _light, lighting_params);
 	expected_color = color_3d(0.1, 0.1, 0.1);
 
 	assert_tuple_eq(expected_color, result_color);
@@ -88,10 +97,12 @@ MU_TEST(light_behind_tst)
 
 MU_TEST(lighting_shadow_tst)
 {
-	eye = vector(0, 0, -1);
-	normal = vector(0, 0, -1);
+	lighting_params.eye = vector(0, 0, -1);
+	lighting_params.normal = vector(0, 0, -1);
+	lighting_params.in_shadow = true;
+	lighting_params.point = _position;
 	_light = point_light(point(0, 0, -10), 1.0, color_rgb(255, 255, 255));
-	result_color = lighting(_material, _light, _position, eye, normal, true);
+	result_color = lighting(_material, _light, lighting_params);
 
 	assert_tuple_eq(color(0.1, 0.1, 0.1), result_color);
 }
