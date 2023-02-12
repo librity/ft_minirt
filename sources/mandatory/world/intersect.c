1240b01506a79c000e8efbf62d0e0423a275c984
Luis <luis.geniole@gmail.com>
2023-02-12 20:20:40 +0100

    Fix intersect_world() norm errors.
    Co-authored-by: Pedro Paulo do Nascimento <pedro-pn@users.noreply.github.com>

--- sources/mandatory/world/intersect.c
+++ sources/mandatory/world/intersect.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 19:08:51 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2023/01/26 18:54:44 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2023/02/12 20:18:50 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,33 +24,37 @@ void	inspect_intersections(t_dlist **intersections)
 	}
 }
 
+static void	add_intersections(t_intxs *result, t_intxs intersections)
+{
+	t_dlist	*last_intersection;
+
+	if (intersections.list == NULL)
+		return ;
+	result->count += intersections.count;
+	if (result->list == NULL)
+	{
+		result->list = intersections.list;
+		return ;
+	}
+	last_intersection = ft_dlstlast(result->list);
+	last_intersection->next = intersections.list;
+	intersections.list->prev = last_intersection;
+}
+
 t_intxs	intersect_world(t_ray ray)
 {
 	t_dlist		*node;
-	t_dlist		*last_intersection;
 	t_object	*object;
-	t_intxs		_intersect;
+	t_intxs		intersections;
 	t_intxs		result;
 
-	result.count = 0;
-	result.list = NULL;
+	result = empty_intersections();
 	node = *objects();
 	while (node != NULL)
 	{
 		object = node->content;
-		_intersect = intersect_object(object, ray);
-		result.count += _intersect.count;
-		if (_intersect.list != NULL)
-		{
-			if (result.list == NULL)
-				result.list = _intersect.list;
-			else
-			{
-				last_intersection = ft_dlstlast(result.list);
-				last_intersection->next = _intersect.list;
-				_intersect.list->prev = last_intersection;
-			}
-		}
+		intersections = intersect_object(object, ray);
+		add_intersections(&result, intersections);
 		node = node->next;
 	}
 	if (result.list != NULL)
